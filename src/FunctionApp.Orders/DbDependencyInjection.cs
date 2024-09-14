d2de1196fa3ae7d392fa451f99d8aa2624d7b704
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2024-09-14 15:14:50 +0700

    add DI for application context
    add DI for application context

---
+++ src/FunctionApp.Orders/DbDependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp.Shared
+{
+    public static class DbDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+       (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // Add services to the container.
+
+            services.AddDbContext<AppDbContext>((sp, options) =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IAppDbContext, AppDbContext>();
+
+            return services;
+        }
+    }
+}
